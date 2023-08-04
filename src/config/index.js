76f76a7fbd6f814d83f19f85432e883728110ecc
不败顽童 <2631416434@qq.com>
2023-08-04 21:31:59 +0800

    refactor:删除无用docker文件和nginx文件

--- src/config/index.js
+++ src/config/index.js
@@ -4,7 +4,7 @@
  * @Author: 不败顽童
  * @Date: 2023-05-26 21:42:34
  * @LastEditors: 不败顽童
- * @LastEditTime: 2023-05-26 22:09:17
+ * @LastEditTime: 2023-08-04 21:31:42
  */
 module.exports = {
   lintOnSave: false,
@@ -13,13 +13,10 @@ module.exports = {
   //   }
   devServer: {
     proxy: {
-      "//back-server": {
+      "/back-server": {
         target: "http://127.0.0.1:9001/",
         // changeOrigin: true,
-      },
-      "/api": {
-        target: "http://s.linweiqin.com/",
-      },
+      }
     },
   },
 };
