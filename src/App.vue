f52e3a5d9cfdce84abc2af1b8b4aafea814f6f35
JavaEdge <sss2855845850@gmail.com>
2023-08-07 13:47:04 +0800

    docs:完整注释 App.vue 文件

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,23 @@
 <template>
     <div id="app" class="course-navigation">
+        <!-- 在div元素内部，依次插入Header组件、router-view组件和Footer组件。-->
         <Header/>
         <router-view class="body"/>
         <Footer/>
     </div>
 </template>
 <script>
-  import Header from './views/Header.vue'; // Import the Header component
-  import Footer from './views/Footer.vue'; // Import the Footer component
+  // 导入Header和Footer组件，这些组件分别位于名为Header.vue和Footer.vue的文件中
+  import Header from './views/Header.vue';
+  import Footer from './views/Footer.vue';
   
   
   export default {
     name: "RecruitNavigation",
     components: {
-      Header, // Register the Header component
-      Footer, // Register the Footer component
-      // ... Rest of your components ...
+      // 在Vue组件中，将Header和Footer组件注册为该组件的子组件
+      Header,
+      Footer,
     },
   
   };
@@ -23,8 +25,8 @@
   
 <style scoped>
     #app {
-        /* position: absolute; */
-        top: 0;
+      // 定位在页面的左上角、设置宽度和高度为100%
+      top: 0;
         left: 0;
         width: 100%;
         height: 100%;
@@ -32,6 +34,7 @@
   .body {
     min-height: 800px;
   }
+  // 最大宽度设置为1600px，并将其水平居中对齐
   .course-navigation {
     max-width: 1600px;
     margin: 0 auto;
