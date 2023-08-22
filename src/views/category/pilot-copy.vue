e33036906b18ebcbfd368bf6c22453af9f3b32ce
1020325258 <1020325258@qq.com>
2023-08-22 10:54:53 +0800

    refactor:修改为原导航页

---
+++ src/views/category/pilot-copy.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="content-class">
+    <PilotSearch></PilotSearch>
+  </div>
+</template>
+
+<script setup>
+import PilotSearch from './pilot-search.vue'
+
+</script>
+
+<style lang="scss" scoped>
+ 
+</style>
