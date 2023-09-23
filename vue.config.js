31eace691364da926219ddaf67c862103a9fcbcc
317439zuohui <1363495704@qq.com>
2023-09-23 22:59:22 +0800

    修改header

--- vue.config.js
+++ vue.config.js
@@ -6,19 +6,17 @@
  * @LastEditors: 不败顽童
  * @LastEditTime: 2023-08-13 22:38:12
  */
+const path = require('path'); // 先引入path模块
 module.exports = {
   lintOnSave: false,
-  //   devServer: {
-  //     proxy: "https://apimusic.linweiqin.com/"
-  //   }
   publicPath: './',
   assetsDir: 'static',
   parallel: false,
   devServer: {
     proxy: {
       "/api/back": {
-        // target: "http://106.52.239.29:8088/",
-        target: "http://localhost:8088/",
+        target: "http://106.52.239.29:8088/",
+        // target: "http://localhost:8088/",
         changeOrigin: true,
         logLevel: "debug",
         pathRewrite:{
@@ -36,4 +34,14 @@ module.exports = {
       // },
     },
   },
+  configureWebpack: {
+    resolve: {
+      alias: {
+      	// 将 ../../assets/img/1.png  转化为 @/1.png
+      	// 将 ../../style/test/1.scss 转化为 @/1.scss
+        '@a': path.join(__dirname, 'src'),
+        '@s': path.join(__dirname, 'src/css'),
+      }
+    }
+  }
 }; 
