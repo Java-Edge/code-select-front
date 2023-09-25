11dc0f9772cd4448a7319c9704e6f983976fc719
317439zuohui <1363495704@qq.com>
2023-09-25 22:56:17 +0800

    feat: footer优化 路由懒加载

--- vue.config.js
+++ vue.config.js
@@ -39,9 +39,23 @@ module.exports = {
       alias: {
       	// 将 ../../assets/img/1.png  转化为 @/1.png
       	// 将 ../../style/test/1.scss 转化为 @/1.scss
-        '@a': path.join(__dirname, 'src'),
+        '@': path.join(__dirname, 'src'),
         '@s': path.join(__dirname, 'src/css'),
       }
     }
-  }
-}; 
+  },
+  css: {
+    loaderOptions: {
+      postcss: {
+        postcssOptions: {
+          plugins: [
+            require("postcss-plugin-px2rem")({
+              rootValue: 16, // 换算基数， 默认100  ，这样的话把根标签的字体规定为1rem为50px,这样就可以从设计稿上量出多少个px直接在代码中写多上px了。
+              minPixelValue: 3, // 设置要替换的最小像素值(3px会被转rem)。 默认 0
+            }),
+          ],
+        },
+      },
+    },
+  },
+};
