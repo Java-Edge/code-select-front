dfabda4d0ab46a4f85fd14493967b926bb4bdfa2
317439zuohui <1363495074@qq.com>
2023-11-19 21:34:09 +0800

    feat:添加锚点和image组件

--- src/main.js
+++ src/main.js
@@ -1,26 +1,29 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-import axios from 'axios';
-import './axios';
-import showdown from "showdown"
-import installElementPlus from './plugins/element'
-import './assets/css/base.css'
-import DisableDevtool from 'disable-devtool';
-
-DisableDevtool({
-	url: 'about:blank',
-	timeOutUrl: 'about:blank',
-	disableMenu: false
-});
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import axios from "axios";
+import "./axios";
+import showdown from "showdown";
+import installElementPlus from "./plugins/element";
+import "./assets/css/base.css";
+import DisableDevtool from "disable-devtool";
+if (process.env.NODE_ENV !== "development") {
+  DisableDevtool({
+    url: "about:blank",
+    timeOutUrl: "about:blank",
+    disableMenu: false,
+  });
+}
 
 const app = createApp(App);
-installElementPlus(app)
+installElementPlus(app);
 
 // 解决bug：页面/路由跳转后，滚动条消失，页面无法滚动
 router.afterEach((to, from, next) => {
-	document.querySelector("body").setAttribute("style", "overflow: auto !important;")
+  document
+    .querySelector("body")
+    .setAttribute("style", "overflow: auto !important;");
 });
 
 app.use(router); // 使用插件方式注册路由
@@ -30,5 +33,4 @@ app.use(showdown); // 注册markdown渲染
 app.config.productionTip = false;
 app.config.globalProperties.$axios = axios; // 在全局配置 $axios
 
-app.mount('#app'); // 挂载 Vue 应用到 DOM 节点
-
+app.mount("#app"); // 挂载 Vue 应用到 DOM 节点
